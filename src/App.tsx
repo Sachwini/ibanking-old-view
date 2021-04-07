8d7974b1c451c39260681e4ab5dc5d203ac93c9f
Sachwini <sachwinikhatri@gmail.com>
2021-04-07 15:37:20 +0545

    display icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Navbar, Row, Col } from "react-bootstrap";
-import FundTransferLimitDetail from "./pages/fundTransferLimitDetail";
-import ChangePassword from "./pages/changePassword";
-import FundTransfer from "./pages/fundTransfer";
-import BillPaymentTransfer from "./pages/billPaymentTransfer";
 import Dashboard from "./pages/dashboard";
 
 function App() {
--- src/pages/configureService.tsx
+++ src/pages/configureService.tsx
@@ -1,5 +1,5 @@
 import { Card } from "react-bootstrap";
-import { RiLockPasswordLine,RiUserStarLine,RiStarSmileLine,RiPieChartBoxFill } from "react-icons/ri";
+import { RiLockPasswordLine,RiUserStarLine,RiStarSmileLine} from "react-icons/ri";
 import { VscReport } from "react-icons/vsc";
 import { FiUserPlus } from "react-icons/fi";
 import { BsShieldLock } from "react-icons/bs";
@@ -7,42 +7,58 @@ import { GrDocumentTime } from "react-icons/gr";
 import { FcQuestions } from "react-icons/fc";
 import { FaPiggyBank } from "react-icons/fa";
 import { FcMoneyTransfer } from "react-icons/fc";
+import { Container, Row, Col } from "react-bootstrap";
 import './iconCard.css' 
 function ConfigureService() {
     return (
         <div>
-            <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
-                <RiLockPasswordLine size="40px" />
-            </Card.Body>
-                <Card.Text className='d-flex justify-content-center align-items-center'>
-                    <span className='cardText'>
-                        Change Password</span>
-                </Card.Text>
-            </Card>  
-            <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'> 
-                <VscReport size="40px"/> 
-            </Card.Body>
-                <Card.Text className='d-flex justify-content-center align-items-center'>
-                    <span className='cardText'>
-                        Report</span>
-                </Card.Text>
-            </Card>
-            <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
-                <FiUserPlus size="40px"/> 
-            </Card.Body>
-                <Card.Text className='d-flex justify-content-center align-items-center'>
-                    <span className='cardText'>
-                        New Account Activate</span>
-                </Card.Text>
-            </Card>
-            <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
-                <BsShieldLock size="40px"/> 
+            <Container fluid>
+                <Row className="g-2"> 
+                    <Col sm={4} md={1}> 
+                        <Card className='cardItem'>
+                            <Card.Body className='d-flex justify-content-center align-items-center'>
+                                <RiLockPasswordLine size="40px" />
+                            </Card.Body>
+                            <Card.Text className='d-flex justify-content-center align-items-center'>
+                                <span className='cardText'>
+                                    Change Password</span>
+                            </Card.Text>
+                        </Card> 
+                    </Col>
+            <Col sm={2} md={1}>         
+                        <Card className='cardItem'>
+                            <Card.Body className='d-flex justify-content-center align-items-center'>
+                                <VscReport size="40px"/> 
+                            </Card.Body>
+                            <Card.Text className='d-flex justify-content-center align-items-center'>
+                            <span className='cardText'>
+                                Report</span>
+                            </Card.Text>
+                        </Card>
+                    </Col>
+             <Col sm={2} md={1}>       
+                        <Card className='cardItem'>
+                            <Card.Body className='d-flex justify-content-center align-items-center'>
+                                <FiUserPlus size="40px"/> 
+                            </Card.Body>
+                            <Card.Text className='d-flex justify-content-center align-items-center'>
+                                <span className='cardText'>
+                                New Account Activate</span>
+                            </Card.Text>
+                        </Card>
+                        </Col>
+            <Col sm={2} md={1}>
+                        <Card className='cardItem'>
+                            <Card.Body className='d-flex justify-content-center align-items-center'>
+                 <BsShieldLock size="40px"/> 
             </Card.Body>
                 <Card.Text className='d-flex justify-content-center align-items-center'>
                     <span className='cardText'>
                         Account Security</span>
                 </Card.Text>
-            </Card>
+                            </Card>
+                            </Col>
+                        <Col sm={2} md={1}>
             <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <RiUserStarLine size="40px"/> 
             </Card.Body>
@@ -50,7 +66,9 @@ function ConfigureService() {
                     <span className='cardText'>
                         Favorite Merchant</span>
                 </Card.Text>
-            </Card>
+                            </Card>
+                            </Col>
+                        <Col sm={2} md={1}>
             <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <RiStarSmileLine size="40px"/> 
             </Card.Body>
@@ -58,7 +76,9 @@ function ConfigureService() {
                     <span className='cardText'>
                         Favorite Account</span>
                 </Card.Text>
-            </Card>
+                            </Card>
+                            </Col>
+                        <Col sm={2} md={1}>
             <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <GrDocumentTime size="40px"/> 
             </Card.Body>
@@ -66,7 +86,9 @@ function ConfigureService() {
                     <span className='cardText'>
                         Schedule Transfer</span>
                 </Card.Text>
-            </Card>
+                        </Card>
+                        </Col>
+                        <Col sm={2} md={1}>
              <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <FcMoneyTransfer size="40px"/> 
             </Card.Body>
@@ -74,7 +96,9 @@ function ConfigureService() {
                     <span className='cardText'>
                         Schedule BillPayment</span>
                 </Card.Text>
-            </Card>
+                        </Card>
+                         </Col>
+                        <Col sm={2} md={1}>
             <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <FcQuestions size="40px"/> 
             </Card.Body>
@@ -82,7 +106,9 @@ function ConfigureService() {
                     <span className='cardText'>
                         Change Security Question</span>
                 </Card.Text>
-            </Card>
+                        </Card>
+                         </Col>
+                        <Col sm={2} md={1}>
              <Card className='cardItem'><Card.Body className='d-flex justify-content-center align-items-center'>
                 <FaPiggyBank size="40px"/> 
             </Card.Body>
@@ -91,6 +117,9 @@ function ConfigureService() {
                         Fixed Deposit</span>
                 </Card.Text>
             </Card>
+                    </Col>
+                </Row>
+            </Container>        
         </div>
     )
 }
