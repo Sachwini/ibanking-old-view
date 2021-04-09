f975967e2cd9b423d4ebe4637e47d569d2d694cf
Nawaraj Jaishi <nawaraj928@gmail.com>
2021-04-09 14:05:13 +0545

    account component change to row basis

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,6 +1,6 @@
 import { AccountName, PageTitle } from "components/page-title";
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { CgNotes } from "react-icons/cg";
 import { IoInformationCircleOutline } from "react-icons/io5";
 import "./Account.css";
@@ -12,15 +12,49 @@ function Account() {
       <PageTitle title="Account" subTitle="manage and view your account" />
       <AccountName name="hamro technology pvt. ltd" accountCode={44356} />
       <Card className="cardBackground">
-        <Card.Body>
-          <Card.Title>
-            <GoFileSubmodule
-              color="orange"
-              size="52px"
-              className="folderIcon"
-            />
-          </Card.Title>
-          <Card.Text>
+        <Card.Body className=" d-flex ">
+          <Row className="custom__row">
+            <Col sm={3} className="custom__col">
+              <Row className="p-0 w-100">
+                <Col lg={3} className="p-0">
+                  <GoFileSubmodule
+                    color="orange"
+                    size="52px"
+                    // className="folderIcon"
+                  />
+                </Col>
+                <Col>
+                  <p>ODA Account</p>
+                  <p>04911000579</p>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm={6} className="custom__col">
+              <Row className="w-100">
+                <Col>
+                  <div>Usable Balance</div>
+                  <div>NPR. 200,000.00</div>
+                </Col>
+                <Col>
+                  <div>Acutal Balance</div>
+                  <div>NPR.200,000.00</div>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm={3} className="justify-content-end">
+              <Row>
+                <Col>
+                  <CgNotes size="23px" />
+                  <span className="iconSpacing">Statment</span>
+                </Col>
+                <Col>
+                  <IoInformationCircleOutline size="26px" />
+                  <span className="iconSpacing">Detail</span>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          {/* <Card.Text>
             <div className="row headerText">
               <div className="col">ODA Account</div>
               <div className="col">Usable Balance</div>
@@ -47,7 +81,7 @@ function Account() {
               <div className="col"></div>
               <div className="col"></div>
             </div>
-          </Card.Text>
+          </Card.Text> */}
         </Card.Body>
       </Card>
     </Container>
