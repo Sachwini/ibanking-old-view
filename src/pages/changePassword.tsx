0d7596fd2ae9f51f0cd999f60c7eab11050cf150
Sachwini <sachwinikhatri@gmail.com>
2021-04-11 12:12:41 +0545

    style change

--- src/pages/changePassword.tsx
+++ src/pages/changePassword.tsx
@@ -1,51 +1,86 @@
 import React from 'react'
 import { Button, Card, Form, Container } from "react-bootstrap";
+import { PageTitle } from "components/page-title/index";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
 
 function ChangePassword() {
     return (
-        <Container className="justify-content-center">
-        <Card style={{ width: '30rem' }}><Card.Body>
-           <Form>
+      <Container className="justify-content-center">
+        <div className="d-flex flex-wrap mt-1">
+          <IoChevronBackCircleOutline size="40px" />
+          <PageTitle
+            title="Change Password"
+            subTitle="Change your current password with a new one"
+          />
+        </div>
+        <div style={{ display: "flex"}}> 
+          <Card style={{ width: "30rem",marginRight:"10px" }}> 
+            <Card.Body>
+              <Form>
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Label className='font-weight-bold'>Current Password</Form.Label>
-                    <Form.Control type="password" placeholder="current password" />
+                  <Form.Label className="font-weight-bold">
+                    Current Password
+                  </Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="current password"
+                  />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Label className='font-weight-bold'>New Password</Form.Label>
-                    <Form.Control type="password" placeholder="new password" />
+                  <Form.Label className="font-weight-bold">
+                    New Password
+                  </Form.Label>
+                  <Form.Control type="password" placeholder="new password" />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Label className='font-weight-bold'>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="confirm password" />
+                  <Form.Label className="font-weight-bold">
+                    Confirm Password
+                  </Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="confirm password"
+                  />
                 </Form.Group>
 
-                <Button className='btn btn-warning' variant="primary" type="submit" block>
-                    Change Password
+                <Button
+                  className="btn btn-warning"
+                  variant="primary"
+                  type="submit"
+                  block
+                >
+                  Change Password
                 </Button>
-                </Form>
+              </Form>
             </Card.Body>
-            </Card>
-            <Card style={{ width: '36rem' }}><Card.Body>
-                <div className='font-weight-bold'>
-                    <ul className="list-unstyled">
-                        <li style={{padding:'14px'}}>Password Policy</li>
-                    <li>
-                        <ul style={{paddingLeft:'16px'}}> 
-                        <li>The overall length of password must be greater than 6 and less than 15</li>
-                        <li>Atleast 1 Number of special characters in password</li>
-                        <li>Atleast 1 Number of digits in password</li>
-                        <li>Atleast 2 Number of lowercase letters in password</li>
-                        <li>Atleast 1 Number of Uppercase letters in password</li>
-                        </ul>
-                    </li> 
+          </Card>
+          <Card style={{ width: "36rem",height:"15rem" }}>
+            <Card.Body>
+              <div className="font-weight-bold">
+                <ul className="list-unstyled">
+                  <li style={{ padding: "14px" }}>Password Policy</li>
+                  <li>
+                    <ul style={{ paddingLeft: "16px" }}>
+                      <li>
+                        The overall length of password must be greater than 6
+                        and less than 15
+                      </li>
+                      <li>
+                        Atleast 1 Number of special characters in password
+                      </li>
+                      <li>Atleast 1 Number of digits in password</li>
+                      <li>Atleast 2 Number of lowercase letters in password</li>
+                      <li>Atleast 1 Number of Uppercase letters in password</li>
                     </ul>
-                    </div>
+                  </li>
+                </ul>
+              </div>
             </Card.Body>
-            </Card>
-        </Container>
-    )
+          </Card>
+        </div>
+      </Container>
+    );
 }
 
 export default ChangePassword
--- src/pages/payment/paymentIndex.tsx
+++ src/pages/payment/paymentIndex.tsx
@@ -1,26 +1,24 @@
-import React from "react";
 import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import { PageTitle } from "components/page-title/index";
 import BillPaymentLog from "./billPaymentLog";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
 
 function Transfer() {
   return (
     <div>
       <div>
-        <Container fluid>
-          <PageTitle
-            title="Reports"
-            subTitle="View your account reports"
-          />
+        <Container>
+          <div className="d-flex flex-wrap mt-1">
+            <IoChevronBackCircleOutline size="40px" />
+            <PageTitle title="Reports" subTitle="View your account reports" />
+          </div>
           <Tabs defaultActiveKey="bpl" id="uncontrolled-tab-example">
             <Tab eventKey="bpl" title="Bill Payment Log">
               <Row>
                 <Col sm={12} md={6}>
                   <BillPaymentLog />
                 </Col>
-                <Col sm={12} md={6}>
-                  {/* <IBFTLimit /> */}
-                </Col>
+                <Col sm={12} md={6}></Col>
               </Row>
             </Tab>
             <Tab eventKey="ips" title="Transfer Log"></Tab>
