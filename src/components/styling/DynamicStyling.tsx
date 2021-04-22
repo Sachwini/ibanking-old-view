081d191486aacd04465f91022ed1bad6ab530762
Sachwini <sachwinikhatri@gmail.com>
2021-04-22 17:07:05 +0545

    Theme problem solved

--- src/components/styling/DynamicStyling.tsx
+++ src/components/styling/DynamicStyling.tsx
@@ -1,16 +1,36 @@
-import styled from "styled-components";
-
+import styled, { css } from "styled-components";
+import { defaultTheme, defaultTextColor } from "./DefaultTheme";
 interface SideBarProps {
   customWidth: string;
-  BgColor: string;
 }
+
+const userThemeColor = {
+  primaryColor: "",
+  secondaryColor: "",
+};
+
+export const theme = {
+  primary: !userThemeColor.primaryColor && defaultTheme.primaryColor,
+  secondary: !userThemeColor.secondaryColor && defaultTheme.secondaryColor,
+  textPrimaryColor: defaultTextColor.primaryTextColor,
+  font: "Cursive",
+  alert: "red",
+};
 
 export const SidebarStyle = styled.div<SideBarProps>`
   width: ${(props) => props.customWidth};
   min-height: 100vh;
   position: fixed;
-  background: ${(props) => props.BgColor};
+  background: ${(props) => props.theme.primary};
   color: white;
   padding: auto;
   z-index: 1200;
+`;
+
+export const SideBarText: any = styled.span`
+  color: ${(props) => props.theme.textPrimaryColor};
+
+  &:hover {
+    color: ${(props) => props.theme.secondary};
+  }
 `;
