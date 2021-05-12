99e46090c4f21121caa2b900989e7f464bf60b87
nawa_raj <nawaraj928@gmail.com>
2021-05-12 14:21:35 +0545

    Bank Transfer ko  error response handle completed

--- src/pages/payment/fund-transfer/BankTransfer.tsx
+++ src/pages/payment/fund-transfer/BankTransfer.tsx
@@ -1,23 +1,18 @@
 import { useEffect, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
-import { fundTransfer } from "./model";
 import { get, post } from "services/AjaxService";
-import { getBankBranches } from "services/BankServices";
 import { Typeahead } from "react-bootstrap-typeahead";
 import { GetAccountNumber } from "helper/CustomerData";
-import { BankList } from "models/BankListType";
+import { bankBranchType, BankList } from "models/BankListType";
 import { apiResponse } from "models/apiResponse";
-import { error } from "node:console";
-
-interface bankBranchType {
-  id: null | string;
-  branchId: string;
-  bankId: string;
-  refBranchId: null | string;
-  branchName: string;
-  enabled: null | string;
-  lastModifiedOn: null | string;
-}
+import {
+  getBankId,
+  getBranchId,
+  getOnlyBankNameList,
+  getOnlyBranchNameList,
+} from "helper/BankTransferHelper";
+import { Loader } from "pages/static/Loader";
+import { toast, ToastContainer } from "react-toastify";
 
 export const BankTransfer = () => {
   const accountNumber = GetAccountNumber();
@@ -28,42 +23,84 @@ export const BankTransfer = () => {
   const [toAccount, setToAccount] = useState<string>("");
   const [DESTAccHolderName, setDESTAccHolderName] = useState<string>("");
   const [DESTBranchList, setDESTBranchList] = useState<bankBranchType[]>([]);
-  const [DESTBranchName, setDESTBranchName] = useState<string>("");
-  const [bankBranchId, setBankBranchId] = useState<string>("");
+  const [DESTBranchName, setDESTBranchName] = useState<string>("null");
   const [transferAmount, setTransferAmount] = useState<string>("");
   const [transctionCharge, setTransctionCharge] = useState<number>(0);
   const [remarks, setRemarks] = useState<string>("");
+  const [handleBranchResponse, setHandleBranchResponse] = useState<string>("");
 
   const [mpin, setMpin] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>();
-  const [isRes, setIsRes] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  // get Bank NameList for Destination Bank Selection
-  let onlyBankName: string[] = [];
-  bankList?.map((list) => {
-    return onlyBankName.push(list.bankName);
-  });
+  // required field Decleration
+  let onlyBankNameList: string[] = [];
+  let selectedBankID: string = "";
+  let onlyBranchNameList: string[] = [];
+  let selectedBankBranchID: string = "";
 
-  // getting selected Bank Name's corrosponding id:
-  const selectedObj = bankList.find(
-    ({ bankName }) => bankName === DESTBankName
-  );
-  let selectedBankID = selectedObj?.bankId;
-  // get bank Crrosponding bank branch
-  let onlyBranchName: string[] = [];
-  DESTBranchList?.map((list) => {
-    return onlyBranchName.push(list.branchName);
-  });
+  // Getting Bank List
+  useEffect(() => {
+    let isSubscribed = true;
 
-  // getting selected Bank Branch Name's corrosponding id:
-  const selectedBranchObj = DESTBranchList.find(
-    ({ branchName }) => branchName === DESTBranchName
-  );
-  const selectedBankBranchID = selectedBranchObj?.branchId;
-  console.log("selected branch id :", selectedBankBranchID);
+    const loadBankList = async () => {
+      const res = await get<apiResponse<BankList[]>>("/api/ips/bank");
+      if (isSubscribed) {
+        setBankList(res.data.details);
+      }
+    };
+    loadBankList();
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
+  // getting Bank List and Selected Bank ID
+  if (bankList.length !== 0 && onlyBankNameList.length === 0) {
+    onlyBankNameList = getOnlyBankNameList(bankList);
+  }
+  if (DESTBankName && selectedBankID === "") {
+    selectedBankID = getBankId(bankList, DESTBankName);
+  }
+
+  // getting Bank Branch List from API
+  const loadBankBranch = async () => {
+    const res = await get<apiResponse<bankBranchType[]>>(
+      `/api/ips/bank/branch?bank_id=${selectedBankID}`
+    );
+    if (
+      res &&
+      res.data.message === "Branch not available." &&
+      res.data.details === null
+    ) {
+      selectedBankBranchID = "null";
+      setHandleBranchResponse("null");
+    } else {
+      setDESTBranchList(res.data.details);
+    }
+  };
+
+  // getting selected Bank Branch List
+  if (
+    selectedBankID &&
+    DESTBranchList !== null &&
+    handleBranchResponse !== "null"
+  ) {
+    loadBankBranch();
+  }
+  // getting Branch ID & handling Branch ID
+  if (DESTBranchList !== null) {
+    onlyBranchNameList = getOnlyBranchNameList(DESTBranchList);
+  }
+  if (DESTBranchName) {
+    selectedBankBranchID = getBranchId(DESTBranchList, DESTBranchName);
+  }
+
+  const handleSelectBank = (e: string[]) => {
+    setDESTBankName(e[0]);
+    setHandleBranchResponse("");
+  };
 
   // storing value into the form data
-
   const formDetaTest = (charge: number) => {
     const formData = new FormData();
 
@@ -85,53 +122,13 @@ export const BankTransfer = () => {
     formData.append("remarks", remarks);
     formData.append("mPin", mpin);
     formData.append("skipValidation", "true");
-    console.log("formData : ", formData);
 
     return formData;
   };
 
-  useEffect(() => {
-    let isSubscribed = true;
-
-    // Bank ListLoad
-    const loadBankList = async () => {
-      const res = await get<apiResponse<BankList[]>>("/api/ips/bank");
-      if (isSubscribed) {
-        setBankList(res.data.details);
-      }
-    };
-
-    const loadBankBranch = async () => {
-      const res = await get<apiResponse<bankBranchType[]>>(
-        `/api/ips/bank/branch?bank_id=${selectedBankID}`
-      );
-      if (isSubscribed) {
-        setDESTBranchList(res.data.details);
-        //   if (res.data.details !== null) {
-        //     setDESTBranch(res.data.details);
-        //   }
-        //   setDESTBranch(["No Branch found"]);
-      }
-    };
-
-    loadBankList();
-    loadBankBranch();
-    return () => {
-      isSubscribed = false;
-    };
-  }, [DESTBankName]);
-
-  const handleReset = (e: any) => {
-    e.preventDefault();
-    setToAccount("");
-    setBankBranchId("");
-    setTransferAmount("");
-    setMpin("");
-  };
-
   const handleSubmit = async (e: any) => {
+    setLoading(true);
     e.preventDefault();
-
     // get transction charge
     const charge = await post<apiResponse<number>>(
       `api/ips/scheme/charge?amount=${transferAmount}&destinationBankId=${selectedBankID}`,
@@ -141,144 +138,169 @@ export const BankTransfer = () => {
       setTransctionCharge(charge.data.details);
       try {
         const data = formDetaTest(charge.data.details);
+
+        //Transfer API fetching start from here
         const bankTransfer = await post<apiResponse<any>>(
           "api/ips/transfer",
           data
         );
-        if (bankTransfer?.statusText) {
+        if (bankTransfer) {
+          toast.success(bankTransfer.data.details);
           console.log("tansfer response : ", bankTransfer.data);
         }
       } catch (error: any) {
         if (error.response) {
           console.log(error.response.data);
+          toast.error(error.response.data.details);
         }
       }
+      setLoading(false);
     }
   };
 
-  // console test
-  console.log("selected bank id : ", selectedBankID);
-  console.log("Transction Charge: ", transctionCharge);
+  const handleReset = (e: any) => {
+    e.preventDefault();
+    setToAccount("");
+    setTransferAmount("");
+    setMpin("");
+  };
 
   return (
-    <Card>
-      <Card.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">From Account</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="from account"
-              name="fromAccount"
-              disabled
-              value={fromAccount}
-              onChange={(e) => setFromAccount(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">Select Bank</Form.Label>
-            <Typeahead
-              id="123"
-              options={onlyBankName}
-              placeholder="Select Destination Bank... "
-              onChange={(e) => setDESTBankName(e[0])}
-            />
-          </Form.Group>
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">Account Number</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="destination bank account number..."
-              name="toAccount"
-              value={toAccount}
-              required
-              onChange={(e) => setToAccount(e.target.value)}
-            />
-            <Form.Text className="text-warning">
-              Please Insure the account number is correct before transaction
-            </Form.Text>
-          </Form.Group>
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">
-              Account Holder Name
-            </Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Account Holder Name..."
-              name="destAccountHilderName"
-              value={DESTAccHolderName}
-              required
-              onChange={(e) => setDESTAccHolderName(e.target.value)}
-            />
-          </Form.Group>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Card>
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">
+                  From Account
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="from account"
+                  name="fromAccount"
+                  disabled
+                  value={fromAccount}
+                  onChange={(e) => setFromAccount(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">
+                  Select Bank
+                </Form.Label>
+                <Typeahead
+                  id="123"
+                  options={onlyBankNameList}
+                  placeholder="Select Destination Bank... "
+                  onChange={handleSelectBank}
+                />
+              </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">
+                  Account Number
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="destination bank account number..."
+                  name="toAccount"
+                  value={toAccount}
+                  required
+                  onChange={(e) => setToAccount(e.target.value)}
+                />
+                <Form.Text className="text-warning">
+                  Please Insure the account number is correct before transaction
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">
+                  Account Holder Name
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Account Holder Name..."
+                  name="destAccountHilderName"
+                  value={DESTAccHolderName}
+                  required
+                  onChange={(e) => setDESTAccHolderName(e.target.value)}
+                />
+              </Form.Group>
 
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">
-              Select Destination Bank Branch
-            </Form.Label>
-            <Typeahead
-              options={onlyBranchName}
-              id="1234"
-              placeholder="Choose destination branch..."
-              onChange={(e) => setDESTBranchName(e[0])}
-            />
-          </Form.Group>
+              {onlyBranchNameList.length === 0 ? (
+                ""
+              ) : (
+                <Form.Group controlId="bankTransfer">
+                  <Form.Label className="font-weight-bold">
+                    Select Destination Bank Branch
+                  </Form.Label>
+                  <Typeahead
+                    options={onlyBranchNameList}
+                    id="1234"
+                    placeholder="Choose destination branch..."
+                    onChange={(e) => setDESTBranchName(e[0])}
+                  />
+                </Form.Group>
+              )}
 
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">Amount</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Amount"
-              name="amount"
-              value={transferAmount}
-              required
-              onChange={(e) => setTransferAmount(e.target.value)}
-            />
-          </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">Amount</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Amount"
+                  name="amount"
+                  value={transferAmount}
+                  required
+                  onChange={(e) => setTransferAmount(e.target.value)}
+                />
+              </Form.Group>
 
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">Remarks</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="remarks..."
-              name="remarks"
-              value={remarks}
-              required
-              onChange={(e) => setRemarks(e.target.value)}
-            />
-          </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">Remarks</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="remarks..."
+                  name="remarks"
+                  value={remarks}
+                  required
+                  onChange={(e) => setRemarks(e.target.value)}
+                />
+              </Form.Group>
 
-          <Form.Group controlId="bankTransfer">
-            <Form.Label className="font-weight-bold">mPin</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="mPin"
-              name="mpin"
-              value={mpin}
-              onChange={(e) => setMpin(e.target.value)}
-            />
-          </Form.Group>
+              <Form.Group controlId="bankTransfer">
+                <Form.Label className="font-weight-bold">mPin</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="mPin"
+                  name="mpin"
+                  value={mpin}
+                  onChange={(e) => setMpin(e.target.value)}
+                />
+              </Form.Group>
 
-          <Button
-            className="btn btn-warning"
-            variant="primary"
-            type="submit"
-            // onClick={handleSubmit}
-          >
-            Submit
-          </Button>
+              <Button
+                className="btn btn-warning"
+                variant="primary"
+                type="submit"
+                // onClick={handleSubmit}
+              >
+                Submit
+              </Button>
 
-          <Button
-            className="btn btn-light"
-            style={{ marginLeft: "20px" }}
-            variant="secondary"
-            type="submit"
-            onClick={handleReset}
-          >
-            Reset
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+              <Button
+                className="btn btn-light"
+                style={{ marginLeft: "20px" }}
+                variant="secondary"
+                type="submit"
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      )}
+      <ToastContainer />
+    </>
   );
 };
