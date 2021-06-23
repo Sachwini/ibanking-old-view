5d2c66a06c8f056027f818a13cbbb709885da440
Sachwini <sachwinikhatri@gmail.com>
2021-06-23 16:31:59 +0545

    notification

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyDbg3seplUur4O_33iD-OG_rIQPj6gftLc",
@@ -8,3 +10,22 @@ export const firebaseConfig = {
   appId: "1:10095689204:web:bf1c04c06052aa36bd4da1",
   measurementId: "G-5XTYCPFEKC",
 };
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging
+  .requestPermission()
+  .then(function () {
+    console.log("Have Permission");
+    return messaging.getToken();
+  })
+  .then(function (token: any) {
+    console.log(token);
+  })
+  .catch(function (err: any) {
+    console.log("Permission Denied");
+  });
+
+messaging.onMessage(function (payload: any) {
+  console.log("onMessage:", payload);
+});
