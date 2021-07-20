b1ded652981ff3c7bf19d56ad5949e9dd4a48035
Nawaraj Jaishi <nawaraj928@gmail.com>
2021-07-20 11:25:28 +0545

    cheques request ko tab change

--- src/pages/request/cheque-request/index.tsx
+++ src/pages/request/cheque-request/index.tsx
@@ -1,94 +1,45 @@
-import { PageTitle } from "components/PageTitle";
 import { useState } from "react";
 import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
 import BlockChequeBook from "./BlockChequeBook";
 import RequestNewCheque from "./RequestNewCheque";
-import "./index.css";
 import StaticBar from "components/StaticBar";
 import { chequeRequestPageTitle } from "static-data/forPageTitle";
 import { forChequeRequest } from "static-data/forBreadCrumb";
-
-const ActiveStyle = {
-  color: "#f58228",
-  borderBottom: "3px solid #f58228",
-  paddingBottom: "8px",
-  letterSpacing: "1px",
-};
-
-const inActiveStyle = {
-  ...ActiveStyle,
-  background: "transparent",
-  borderBottom: "",
-  color: "inherit",
-};
+import { CustomTabs } from "styling/TabsStyling";
 
 function ChequeRequest() {
-  const [key, setKey] = useState<string | boolean | null>("cheque_Request");
+  const [key, setKey] = useState<any>("new_cheque_Request");
 
   return (
-    <div>
-      <Container>
-        <StaticBar
-          pageTitle={chequeRequestPageTitle}
-          breadCrumbData={forChequeRequest}
-        />
+    <Container>
+      <StaticBar
+        pageTitle={chequeRequestPageTitle}
+        breadCrumbData={forChequeRequest}
+      />
 
-        <Tab.Container activeKey={key} onSelect={(key) => setKey(key)}>
-          <div className="custom__tab">
-            <Nav.Link
-              eventKey="cheque_Request"
-              style={key === "cheque_Request" ? ActiveStyle : inActiveStyle}
-            >
-              <span className="tab__text">Cheque Request</span>
-            </Nav.Link>
+      <CustomTabs
+        id="cheque-request"
+        activeKey={key}
+        onSelect={(key: any) => setKey(key)}
+      >
+        <Tab eventKey="new_cheque_Request" title="Request New Cheque">
+          <Row>
+            <Col sm={12} lg={6}>
+              <RequestNewCheque />
+            </Col>
+          </Row>
+        </Tab>
 
-            <Nav.Link
-              eventKey="block"
-              style={key === "block" ? ActiveStyle : inActiveStyle}
-            >
-              <span className="tab__text">Block Cheque</span>
-            </Nav.Link>
-          </div>
-
-          <Tab.Content style={{ padding: "20px", paddingLeft: "0" }}>
-            <Tab.Pane eventKey="cheque_Request">
-              <Row>
-                <Col sm={12} md={6}>
-                  <RequestNewCheque />
-                </Col>
-                <Col sm={12} md={6}></Col>
-              </Row>
-            </Tab.Pane>
-
-            <Tab.Pane eventKey="block">
-              <Row>
-                <Col sm={12} md={6}>
-                  <BlockChequeBook />
-                </Col>
-              </Row>
-            </Tab.Pane>
-          </Tab.Content>
-        </Tab.Container>
-      </Container>
-    </div>
+        <Tab eventKey="block_cheque" title="Block Cheque">
+          <Row>
+            <Col sm={12} lg={6}>
+              <BlockChequeBook />
+            </Col>
+          </Row>
+        </Tab>
+      </CustomTabs>
+    </Container>
   );
-  // return (
-  //   <Container>
-  //     <PageTitle
-  //       title="Cheque inquiry"
-  //       subTitle="Request for new cheque book or block the current one"
-  //     />
-  //     <hr />
-  //     <Row>
-  //       <Col sm={12} md={6}>
-  //         <RequestNewCheque />
-  //       </Col>
-  //       <Col sm={12} md={6}>
-  //         <BlockChequeBook />
-  //       </Col>
-  //     </Row>
-  //   </Container>
-  // );
 }
 
 export default ChequeRequest;
